b8bb4b20c5565887f17884e96a99956756be0ace
Rob O'Brien <rob@zepoid.com>
2018-05-30 22:14:05 -0400

    Align grid breakpoints to Boostrap

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { LayoutModule } from '../layouts/layouts.module';
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    LayoutModule,
+  ],
+})
+export class PageModule { }
